9df96c46a05be6901420e0e802e48b78e72eeac0
Kevin Emunds <Kevin.Emunds_1@student.hs-rm.de>
2022-05-26 16:07:57 +0200

    init

---
+++ spass/src/main/java/de/hsrm/mi/swt/spass/SpassApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.swt.spass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpassApplication.class, args);
+	}
+
+}
