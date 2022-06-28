27f7af396eade467dea9632bfd416c44f81890a0
Kevin Emunds <Kevin.Emunds_1@student.hs-rm.de>
2022-06-28 17:21:41 +0200

    Validator Chaos

--- spass/src/main/java/de/hsrm/mi/swt/spass/geschaeftslogik/validiererVerwaltung/validierer/ValidateError.java
+++ spass/src/main/java/de/hsrm/mi/swt/spass/geschaeftslogik/validiererVerwaltung/validierer/ValidateError.java
@@ -1,7 +1,8 @@
 package de.hsrm.mi.swt.spass.geschaeftslogik.validiererVerwaltung.validierer;
 
-public class ValidateKompetenzenError extends ValidateError{
-    ValidateKompetenzenError(String e){
+public class ValidateError extends RuntimeException {
+    ValidateError(String e){
         super(e);
     }
+    
 }
---
+++ spass/src/main/java/de/hsrm/mi/swt/spass/geschaeftslogik/validiererVerwaltung/validierer/ValidateFortschrittsregelError.java
@@ -0,0 +1,8 @@
+
+    package de.hsrm.mi.swt.spass.geschaeftslogik.validiererVerwaltung.validierer;
+
+    public class  ValidateFortschrittsregelError extends ValidateError{
+        ValidateFortschrittsregelError (String e){
+            super(e);
+        }
+    }
